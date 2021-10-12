da575cb77ab7b426ade5ce1923aae13d1746e7aa
Tani Kaye <1tanikaye@gmail.com>
2021-10-12 17:46:28 -0400

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+  return Math.abs(blocks - 42);
+}
+
+function distanceFromHqInFeet (blocked) {
+  var blocked = Math.abs(blocked - 42);
+  return (blocked * 264);
+}
+
+function distanceTravelledInFeet(num1, num2) {
+return (Math.abs(num1 - num2) * 264);
+}
+
+function calculatesFarePrice(start, end) {
+  var distance = (Math.abs(start - end) * 264);
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return (distance - 400) * .02;
+  } else if (distance > 2000 && distance < 2500) {
+    return 25
+  } else if (distance > 2500) {
+    return 'cannot travel that far';
+  }
+}
